86320c0da0af98e1c4bc7d575ee5941a95f0ee28
Pedro Mello <pedrocmello.bsi@gmail.com>
2020-01-05 01:51:38 -0300

    (add) custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './404.css'
+
+export default () => {
+	return (
+		<figure>
+			<figcaption>
+				<span class="e"></span>
+				<span class="r"></span>
+				<span class="r"></span>
+				<span class="o"></span>
+				<span class="r"></span>
+				<span class="_4"></span>
+				<span class="_0"></span>
+				<span class="_4"></span>
+				<span class="n"></span>
+				<span class="o"></span>
+				<span class="t"></span>
+				<span class="f"></span>
+				<span class="o"></span>
+				<span class="u"></span>
+				<span class="n"></span>
+				<span class="d"></span>
+			</figcaption>
+			<div role="link" class="linkDiv">
+				<a href="/" class="goBack">Me tira daqui!!</a>
+			</div>
+		</figure>
+	)
+}
